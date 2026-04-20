2120ef017e42b93fcf9f55110d67e9908c505693
Abraham Pilar Osman <91863616+Presudeos@users.noreply.github.com>
2026-04-20 15:26:45 +0700

    ignore

--- fix-escapes-4.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let text = fs.readFileSync('src/components/dashboard/CapexGanttMonitor.tsx', 'utf8');
-
-// The issue was `$\{currentMonth}`.
-// Let's just fix all of them with a clean string replace
-text = text.replace(/\$\\\{/g, '${');
-
-fs.writeFileSync('src/components/dashboard/CapexGanttMonitor.tsx', text);
-console.log('replaced $\\{ with ${');
